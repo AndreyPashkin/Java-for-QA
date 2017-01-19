f7028a454af2d4c82e099967d00aa944e6994aa8
AndreyPashkin <druidan@mail.ru>
2017-01-19 16:57:22 +0300

    Выполнение задания 2.4.
    Выполнение задания 2.4. Метод distance добавлен в класс Point.

--- sandbox/src/main/java/ru/javaqa/sandbox/Start.java
+++ sandbox/src/main/java/ru/javaqa/sandbox/Start.java
@@ -5,18 +5,11 @@ package ru.javaqa.sandbox;
  */
 public class Start {
     public static void main(String[] args) {
-        Point p1 = new Point();
-        Point p2 = new Point();
-        p1.x = 0;
-        p1.y = 0;
-        p2.x = 4;
-        p2.y = 3;
+        Point p1 = new Point(1, 1);
+        Point p2 = new Point(5, 4);
 
-        System.out.println(distance(p1, p2));
+        System.out.println(p1.distance(p2));
 
     }
 
-    public static double distance(Point p1, Point p2) {
-        return Math.sqrt((p2.x - p1.x) * (p2.x - p1.x) + (p2.y - p1.y) * (p2.y - p1.y));
-    }
 }
